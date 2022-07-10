011ea1927bc205aa80d457ab66e1cc0f0b951efc
Anderso Henrique <dantasanderson57@hotmail.com>
2022-07-10 01:54:53 -0300

    Feito algumas questões

--- src/main/java/com/example/computadores/controller/ComputadorController.java
+++ src/main/java/com/example/computadores/controller/ComputadorController.java
@@ -6,15 +6,15 @@ import com.example.computadores.service.FileStorageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class ComputadorController {
@@ -28,7 +28,15 @@ public class ComputadorController {
     }
 
     @GetMapping("/")
-    public String getHome(){
+    public String getComputadorHome(Model model, HttpServletResponse response){
+
+        List<Computador> computador = service.findAll();
+        model.addAttribute("computador", computador);
+
+        Cookie cookie = new Cookie("visita","cookie-value");
+        cookie.setMaxAge(60*60*24);
+        response.addCookie(cookie);
+
         return "index";
     }
 
@@ -39,6 +47,32 @@ public class ComputadorController {
 
         return "cadastrar";
 
+    }
+
+    @GetMapping("editar/{id}")
+    public String getEditarComputador(Model model, @PathVariable Long id, RedirectAttributes redirectAttributes){
+
+        Computador computador = service.findById(id);
+        model.addAttribute("computador", computador);
+
+        redirectAttributes.addAttribute("msg2", "Cadastro atualizado com sucesso");
+        return "cadastrar";
+    }
+
+    @GetMapping("deletar/{id}")
+    public String getDeletarComputador(@ModelAttribute Computador c, Model model, @PathVariable Long id){
+        System.out.println(c.getDescricao());
+        Computador computador = service.findById(c.getId());
+        computador.setDescricao(c.getDescricao());
+        computador.setImagem(c.getImagem());
+        computador.setMarca(c.getMarca());
+        computador.setModelo(c.getModelo());
+        computador.setPreco(c.getPreco());
+        computador.setDeletd(false);
+        service.update(computador);
+        List<Computador> computadores = service.findAll();
+        model.addAttribute("computador", computadores);
+        return "index";
     }
 
     @PostMapping("salvar")
@@ -63,5 +97,19 @@ public class ComputadorController {
             redirectAttributes.addAttribute("msg", "Cadastro realizado com sucesso");
             return "redirect:/";
         }
+    }
+
+    @GetMapping("/adicionarCarrinho/{id}")
+    public String getAddCarrinho(Model model, @PathVariable Long id){
+
+        Computador computador = service.findById(id);
+        model.addAttribute("computador", computador);
+
+        return "cadastrar";
+    }
+
+    @GetMapping("/vercarrinho")
+    public String getVerCarrinho(Model model){
+        return "vercarrinho";
     }
 }
--- src/main/java/com/example/computadores/domain/Computador.java
+++ src/main/java/com/example/computadores/domain/Computador.java
@@ -1,5 +1,6 @@
 package com.example.computadores.domain;
 
+import com.example.computadores.errorhandling.ApiMenssages;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 @Entity
@@ -20,10 +22,12 @@ public class Computador {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = ApiMenssages.ERRO_VAZIO)
     String modelo;
     String marca;
     String preco;
     String imagem;
     String descricao;
+    Boolean deletd;
 
 }
